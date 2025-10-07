0b702488f62ada1c1f7b8eb91d6dae6569c96470
JulienNal <naljs@mail.uc.edu>
2025-10-07 14:49:41 -0400

    New commit

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,10 @@
-public class Task3 {
+public class Task3
+{
+    public static void main(String[] args)
+    {
+        for (int h = 0; h <= 18; h+=3) {
+            System.out.println(h);
+        }
+
+    }
 }
